abff82ec48c23e306f374cd7b52d2af2f564f0ea
JCEEE <0xjceee@proton.me>
2026-03-11 17:48:26 +0000

    init repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<main>
+	<section class="grid h-screen w-screen place-items-center overflow-hidden">
+		<img
+			src="YCWTT.jpg"
+			class="max-h-screen max-w-full object-contain"
+			alt="yoga circus with the trees"
+		/>
+	</section>
+</main>
